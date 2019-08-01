bfa46e381e0eb9ef59854d97d0349f6c9e44b6ee
ProgrammingTitan <programmingtitan@gmail.com>
2019-08-01 03:18:53 -0700

    backend

--- server.js
+++ server.js
@@ -34,11 +34,13 @@ app.use('/api/greetings', greetings);
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
-
-    app.get( '*' , (req,res) => {
-        res.sendFile(path.resolve(__dirname, '' , 'build', 'index.html'));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
