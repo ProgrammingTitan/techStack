c34b489093f047f28ff820a1d6f6e9e219c7ae6f
ProgrammingTitan <programmingtitan@gmail.com>
2019-08-01 15:35:39 -0700

    backend

--- server.js
+++ server.js
@@ -18,13 +18,12 @@ const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-    .connect(db, {
-        useNewUrlParser:true,
-        useCreateIndex: true
-    })
-    .then( () => console.log('MongoDB Connected...'))
-    .catch( err => console.log(err));
-
+  .connect(db, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/headlines', headlines);
@@ -34,16 +33,17 @@ app.use('/api/greetings', greetings);
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'index.html'));
-    });
-  }
-  
-  const port = process.env.PORT || 5000;
-  
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
