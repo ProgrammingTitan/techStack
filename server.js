6aa90c4f364d6ec4d5f1855f28ca1e6d8290b128
ProgrammingTitan <programmingtitan@gmail.com>
2019-08-01 04:00:25 -0700

    handle

--- server.js
+++ server.js
@@ -35,16 +35,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('build'));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
   
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve('build', 'index.html'));
-//     });
-// }
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const port = process.env.PORT || 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`));
